8377edcddf871e3749fea43f85ae6a04359822e1
ManuelDeLeon <manueldeleon@gmail.com>
2016-11-26 04:50:34 -0700

    remove promise

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-require('es6-promise').polyfill()
+//require('es6-promise').polyfill()
 var isDevBuild = process.argv.indexOf('--env.prod') < 0;
 var path = require('path');
 var webpack = require('webpack');
